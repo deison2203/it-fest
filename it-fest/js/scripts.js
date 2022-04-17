94151a6f5e069ab6f6644ff2b5cdb93fe3e31622
deison2203 <88599320+deison2203@users.noreply.github.com>
2022-04-17 20:36:23 +0300

    Add files via upload

---
+++ it-fest/js/scripts.js
@@ -0,0 +1,47 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+var lastResFind = "";
+var copy_page = "";
+
+function TrimStr(s) {
+  s = s.replace(/^\s+/g, '');
+  return s.replace(/\s+$/g, '');
+}
+
+function FindOnPage(inputId) {
+  var obj = window.document.getElementById(inputId);
+  var textToFind;
+ 
+  if (obj) {
+    textToFind = TrimStr(obj.value);
+  } else {
+    alert("Введенная фраза не найдена");
+    return;
+  }
+  if (textToFind == "") {
+    alert("Вы ничего не ввели");
+    return;
+  }
+
+  if (document.body.innerHTML.indexOf(textToFind) == "-1")
+    alert("Ничего не найдено, проверьте правильность ввода!");
+
+  if (copy_page.length > 0)
+    document.body.innerHTML = copy_page;
+  else copy_page = document.body.innerHTML;
+
+
+  document.body.innerHTML = document.body.innerHTML.replace(eval("/name=" + lastResFind + "/gi"), " ");
+  document.body.innerHTML = document.body.innerHTML.replace(eval("/" + textToFind + "/gi"), "<a name=" + textToFind + " style='background:red'>" + textToFind + "</a>"); //Заменяем найденный текст ссылками с якорем;
+  lastResFind = textToFind; 
+  window.location = '#' + textToFind; 
+}
